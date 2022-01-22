af86c1948b6a2a088c404117b39e2a6549139500
Falcon <falcon@Falcons-MBP.lan>
2022-01-22 23:11:07 +0800

    -

--- GitSearch/CocabuStart.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/jython
-# -*- coding: utf-8 -*-
-
-import os
-os.system("jython Bootstrap.py ./FrontEnd/server.py")
--- GitSearch/Indexer.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-#from Indexer._QuestionIndexer import question_main
-from Indexer._AnswerIndexer import answer_main
-#from Indexer._GitHubIndexer import github_main
-
-#question_main()
-answer_main()
-#github_main()
